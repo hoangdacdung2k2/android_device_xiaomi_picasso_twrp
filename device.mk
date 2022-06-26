ada88a9cf8df6437d0cb67bb15a81f94fe3aef00
hoangdacdung2k2 <99011159+hoangdacdung2k2@users.noreply.github.com>
2022-06-26 16:17:35 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -35,3 +35,11 @@ PRODUCT_TARGET_VNDK_VERSION := 31
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
